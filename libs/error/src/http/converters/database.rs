fd1f5fea2ae1c0e93b54f8943781481b69b9b6b5
ogedi001 <ogedifavour2@gmail.com>
2026-02-13 00:49:55 +0100

    feat: error libs, http  module

--- libs/error/src/http/converters/database.rs
+++ libs/error/src/http/converters/database.rs
@@ -3,6 +3,12 @@ use crate::http::ApiError;
 
 impl From<DatabaseError> for ApiError {
     fn from(error: DatabaseError) -> Self {
-        ApiError::internal("Database error occurred")
+        ApiError::internal(format!("Database error occurred: {}", error.message)).with_details(
+            serde_json::json!({
+                "message": error.message,
+                "code": error.code,
+                "source": "database"
+            }),
+        )
     }
 }
